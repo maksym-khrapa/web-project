ae3100d784f14d1aeeb88726f1cc88a3d1314f88
maksym-khrapa <maksym.khrapa@gmail.com>
2025-05-23 20:37:16 +0300

    Update tasks-03.js

--- tasks-03.js
+++ tasks-03.js
@@ -5,8 +5,8 @@ function circleCircumference(circle) {
 
 // https://www.codewars.com/kata/5722b3f0bd5583cf44001000/train/javascript
 function giveMeFive(obj) {
-  var arr = [];
-  for (var key in obj) {
+  let arr = [];
+  for (let key in obj) {
     if (key.length == 5) arr.push(key);
     if (obj[key].length == 5) arr.push(obj[key]);
   }
